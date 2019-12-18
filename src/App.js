a864eabae3fc19b941550c0b336241c7a5d45436
nikkoe82 <nikkoelliott@gmail.com>
2019-12-18 16:02:37 -0600

    Add new logo and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import nikkosMugTitle from './nikkosmugtitle.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={nikkosMugTitle} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. coo?
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
