f556b036bfff54382f732c45f3a423223c93bd68
nikkoe82 <nikkoelliott@gmail.com>
2019-12-26 15:28:33 -0600

    Add files and mild code clean up

--- src/App.js
+++ src/App.js
@@ -115,8 +115,8 @@ export default class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={nikkosMugTitle} className="App-logo" alt="logo" />
-          <p style={{ width: '50%' }}>
-            Click on a mug below to learn more about it. Once you've decided
+          <p style={{ width: '60%' }}>
+            Browse through the mugs below and click on one to learn more about it. Once you've decided
             which mug you want to use, click "Select" and then mug it up!
           </p>
           <div className="slider">
--- src/Slide.js
+++ src/Slide.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Mugs from './Mugs.js';
 
 function mugLightbox_close() {
-  console.log('hey')
   document.getElementById('mugLight').style.display = 'none';
   document.getElementById('mugFade').style.display = 'none';
 }
 
 function mugLightbox_open() {
-  console.log('open')
   document.getElementById('mugLight').style.display = 'block';
   document.getElementById('mugFade').style.display = 'block';
 }
@@ -21,7 +19,7 @@ const Slide = ({ frontImage, image }) => {
     backgroundRepeat: 'no-repeat',
     float: 'left',
   };
-  
+
   return (
     <div>
     <div className="slide" style={styles} onClick={mugLightbox_open}>
