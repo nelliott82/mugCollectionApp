3a90781a69b6f71cef44db3c23035b5f700142c2
nikkoe82 <nikkoelliott@gmail.com>
2020-02-10 14:17:51 -0600

    Add clean up to code

--- public/pixieDust.js
+++ public/pixieDust.js
@@ -28,19 +28,6 @@ a=(ns||n6)?'':'all.';
 n6r=(n6)?'")':'';
 s=(ns)?'':'.style';
 
-// if (ns){
-// 	for (i = 0; i < n; i++)
-// 		document.write('<layer name="dots'+i+'" top=0 left=0 width='+i/2+' height='+i/2+' bgcolor=#ff0000></layer>');
-// }
-
-// if (ie)
-// 	document.write('<div id="con" style="position:absolute;top:0px;left:0px"><div style="position:relative">');
-
-// if (ie||n6){
-// 	for (i = 0; i < n; i++)
-// 		document.write('<div id="dots'+i+'" style="position:absolute;top:0px;left:0px;width:'+i/2+'px;height:'+i/2+'px;background:#ff0000;font-size:'+i/2+'"></div>');
-// }
-
 if (ie)
 	document.write('</div></div>');
 (ns||n6)?window.captureEvents(Event.MOUSEMOVE):0;
@@ -52,39 +39,6 @@ function Mouse(evnt){
 }
 
 (ns)?window.onMouseMove=Mouse:document.onmousemove=Mouse;
-
-// function animate(){
-
-// 	o=(ns||n6)?window.pageYOffset:0;
-
-// 	if (ie)con.style.top=document.body.scrollTop + 'px';
-
-// 	for (i = 0; i < n; i++){
-
-// 		var temp1 = eval(d+a+"dots"+i+n6r+s);
-
-// 		randcolours = colours[Math.floor(Math.random()*colours.length)];
-
-// 		(ns)?temp1.bgColor = randcolours:temp1.background = randcolours; 
-
-// 		if (i < n-1){
-
-// 			var temp2 = eval(d+a+"dots"+(i+1)+n6r+s);
-// 			temp1.top = parseInt(temp2.top) + 'px';
-// 			temp1.left = parseInt(temp2.left) + 'px';
-
-// 		} 
-// 		else{
-
-// 			temp1.top = y+o + 'px';
-// 			temp1.left = x + 'px';
-// 		}
-// 	}
-
-// 	setTimeout("animate()",10);
-// }
-
-// animate();
 
 window.onload=function() { if (document.getElementById) {
 	var i, rats, rlef, rdow;
